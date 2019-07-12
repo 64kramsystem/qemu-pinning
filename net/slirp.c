25da296cb165f524d429644960d8ed168269fbb5
Saverio Miroddi <saverio.pub2@gmail.com>
2019-07-12 11:23:36 +0200

    Allow symlinks in Samba share

--- net/slirp.c
+++ net/slirp.c
@@ -867,6 +867,8 @@ static int slirp_smb(SlirpState* s, const char *exported_dir,
             "printing = bsd\n"
             "disable spoolss = yes\n"
             "usershare max shares = 0\n"
+            "follow symlinks = yes\n"
+            "wide links = yes\n"
             "[qemu]\n"
             "path=%s\n"
             "read only=no\n"
